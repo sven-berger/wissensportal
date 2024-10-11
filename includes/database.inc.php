d54029406cc814bb2930e719de90ebe03009e0cd
sven-berger <bergersvenoliver@gmail.com>
2024-10-11 08:36:47 +0200

    main: säuberungsaktion

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -11,17 +11,3 @@ try {
     echo 'Es liegt ein Problem bei der Datenbankverbindung vor: ' . "<br/><br/>";
     echo "<pre>" . var_dump($e) . "</pre>";
 }
-
-
-function getMariaDBVersion($connection) {
-        $stmt = $connection->query('SELECT VERSION()');
-        $version = $stmt->fetchColumn();
-
-        if (preg_match('/(MariaDB\s+\d+\.\d+\.\d+)/', $version, $matches)) {
-            return $matches[1];
-        } else {
-            return $version;
-        }
-}
-
-?>
