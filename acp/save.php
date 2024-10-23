58bb1eeb9c84f8ba85c52d88aa5c2fcc191c4084
sven-berger <bergersvenoliver@gmail.com>
2024-10-23 13:07:33 +0200

    Update save.php

--- acp/save.php
+++ acp/save.php
@@ -4,23 +4,24 @@ $bereich = 'Administrationsbereich';
 $pageTitle = 'Snippet abschicken';
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/acp/includes/header.inc.php");
 
+$url = $_POST['url'];
 $title = $_POST['title'];
 $description = $_POST['description'];
 $php_snippet = $_POST['php_snippet'];
 $python_snippet = $_POST['python_snippet'];
 $javascript_snippet = $_POST['javascript_snippet'];
-$url = $_POST['url'];
 
-$sql = "INSERT INTO snippets (title, description, php_snippet, python_snippet, javascript_snippet, url) 
-        VALUES (:title, :description, :php_snippet, :python_snippet, :javascript_snippet, :url)";
+
+$sql = "INSERT INTO snippets (url, title, description, php_snippet, python_snippet, javascript_snippet) 
+        VALUES (:url, :title, :description, :php_snippet, :python_snippet, :javascript_snippet)";
 $stmt = $connection->prepare($sql);
 $stmt->execute([
+    ':url' => $url,
     ':title' => $title,
     ':description' => $description,
     ':php_snippet' => $php_snippet,
     ':python_snippet' => $python_snippet,
-    ':javascript_snippet' => $javascript_snippet,
-    ':url' => $url
+    ':javascript_snippet' => $javascript_snippet
 ]);
 
 header("Location: https://wissensportal.codevoyage.de/index.php");
