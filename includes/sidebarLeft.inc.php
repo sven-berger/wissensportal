a6c16c46ad4f4a557671d19247647d72bfbb6427
sven-berger <bergersvenoliver@gmail.com>
2024-10-24 10:36:32 +0200

    Update

--- includes/sidebarLeft.inc.php
+++ includes/sidebarLeft.inc.php
@@ -2,34 +2,46 @@
 <div class="boxCapital" style="margin-top: 20px;">
     <p>Variablen</p>
 </div>
-    <section class="sidebarBox">
-        <div class="boxContent">
-            <ul>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=hallo-welt">Hello World!</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=mathematische-operatoren">Mathematische Operatoren</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=strings-wieder-verschoenern">Strings wieder verschönern</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=werte-in-strings-grossschreiben">Werte in Strings großschreiben</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=werte-in-strings-kleinschreiben">Werte in Strings kleinschreiben</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=variablen-zusammenfassen_verkuerzen"><span class="label">Python</span> Variablen zusammenfassen/verkürzen</a></li>
-            </ul>
-        </div>
-    </section>
+<section class="sidebarBox">
+    <div class="boxContent">
+        <ul>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=hallo-welt">Hello World!</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=mathematische-operatoren">Mathematische Operatoren</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=strings-wieder-verschoenern">Strings wieder verschönern</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=werte-in-strings-grossschreiben">Werte in Strings großschreiben</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=werte-in-strings-kleinschreiben">Werte in Strings kleinschreiben</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=variablen-zusammenfassen_verkuerzen"><span class="label">Python</span> Variablen zusammenfassen/verkürzen</a></li>
+        </ul>
+    </div>
+</section>
 
 <div class="boxCapital">
-    <p>Arrays/Listen</p>
+    <p>Arrays / Listen</p>
 </div>
-    <section class="sidebarBox">
-        <div class="boxContent">
-            <ul>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=einfache_r-array_liste">Einfache(r) Array / Liste</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=laenge-des-arrays_der-liste">Länge des Arrays / der Liste</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=array_liste-bearbeiten">Array / Liste bearbeiten</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=kleinste-und-groesste-zahl-aus-array_aus-der-liste">Kleinste und größte Zahl aus dem Array / der Liste</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=zahlen-im-array_in-der-liste-zusammenrechnen">Zahlen im Array / in der Liste zusammenrechnen</a></li>
-                <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=arrays_listen-sortieren">Arrays / Listen sortieren</a></li>
-            </ul>
-        </div>
-    </section>
+<section class="sidebarBox">
+    <div class="boxContent">
+        <ul>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=einfache_r-array_liste">Einfache(r) Array / Liste</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=laenge-des-arrays_der-liste">Länge des Arrays / der Liste</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=array_liste-bearbeiten">Array / Liste bearbeiten</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=kleinste-und-groesste-zahl-aus-array_aus-der-liste">Kleinste und größte Zahl aus dem Array / der Liste</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=zahlen-im-array_in-der-liste-zusammenrechnen">Zahlen im Array / in der Liste zusammenrechnen</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=arrays_listen-sortieren">Arrays / Listen sortieren</a></li>
+        </ul>
+    </div>
+</section>
+<div class="boxCapital">
+    <p>for-Schleife</p>
+</div>
+<section class="sidebarBox">
+    <div class="boxContent">
+        <ul>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=for-schleife">Die for-Schleife</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=array_listen-zusammensetzen">Array / Liste zusammensetzen</a></li>
+            <li><a href="https://wissensportal.codevoyage.de/index.php?snippet=zahlen-von-1bis100-mit-for-in-array_liste">Zahlen von 1 - 100 mit for im Array / in der Liste</a></li>
+        </ul>
+    </div>
+</section>
 </nav>
 
 
--- snippets/2_arrays/einfache-liste.py
+++ snippets/2_arrays/einfache-liste.py
@@ -2,12 +2,15 @@ tiere = ["Wal", "Gans", "Affe", "Nashorn", "Zebra", "Esel"]
 print(tiere)
 print(tiere[1])
 print(tiere[0], tiere[1], tiere[2], tiere[3], tiere[4], tiere[5])
+
 #Das letzte Element
 print(tiere[-1])
+
 #Das Vorletzte etc. ...
 print(tiere[-2])
 
-### Per : entscheiden was VON - BIS rausgegriffen werden soll - Ohne Wert nach : bedeutet: Bis zum Ende. ###
+### Per Doppelpunkt entscheiden was VON - BIS rausgegriffen werden soll - Ohne Wert nach : bedeutet: Bis zum Ende. ###
+
 ## Nashorn, Zebra, Esel
 print(tiere[-3:])
 
