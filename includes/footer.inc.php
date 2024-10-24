129aafc022fbb7d37aae5f972339c54be3e42508
sven-berger <bergersvenoliver@gmail.com>
2024-10-24 11:09:57 +0200

    Update footer.inc.php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -4,6 +4,7 @@
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/sidebarLeft.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/sidebarRight.inc.php");
 ?>
 
 </div>
