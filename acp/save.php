4026806a6448ffb3ef0efe7d8e440a56cc5807a0
sven-berger <bergersvenoliver@gmail.com>
2024-10-24 12:44:34 +0200

    Test

--- acp/save.php
+++ acp/save.php
@@ -26,7 +26,7 @@ $stmt->execute([
     ':javascript_snippet' => $javascript_snippet
 ]);
 
-header("Location: https://wissensportal.codevoyage.de/index.php?id=snippet=" . $snippet['id']);
+header("Location: https://wissensportal.codevoyage.de/index.php?id=snippet=" . $snippet['url']);
 
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.inc.php");
 ?>
